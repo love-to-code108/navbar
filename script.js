33b5f725a721908b26866485b0ea42b57000f817
love-to-code108 <love.to.code108@gmail.com>
2023-06-27 23:43:43 +0530

    finally done

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-const currentUrl = location.href;
-console.log(currentUrl);
-
-
-if(currentUrl == "http://127.0.0.1:5500/index.html" ){
-    const home = document.getElementsByClassName("whiteUrl");
-    home[0].style.backgroundColor = "white";
+{
+    const currentUrl = location.href;
     console.log(currentUrl);
+
+
+    if (currentUrl == "http://127.0.0.1:5500/index.html") {
+        const home = document.getElementsByClassName("whiteUrl");
+        home[0].style.backgroundColor = "white";
+        console.log(currentUrl);
+    }
+
+
+    else if (currentUrl == "http://127.0.0.1:5500/profiles.html") {
+        const home = document.getElementsByClassName("whiteUrl");
+        home[1].style.backgroundColor = "white";
+        console.log(currentUrl);
+    }
 }
 
 
-else if(currentUrl == "http://127.0.0.1:5500/profiles.html" ){
-    const home = document.getElementsByClassName("whiteUrl");
-    home[1].style.backgroundColor = "white";
-    console.log(currentUrl);
+
+const openUrl = (a)=>{
+    if(a == 1){
+        window.open("http://127.0.0.1:5500/index.html","_self");
+
+
+    }else if(a == 2){
+        window.open("http://127.0.0.1:5500/profiles.html","_self");
+
+
+    }else if(a == 3){
+
+    }
 }
+
