031b5975ce71a72279392c6e2d7e7afd2a301143
Biswanath Mukherjee <114847271+love-to-code108@users.noreply.github.com>
2023-06-28 00:06:21 +0530

    Update script.js
    fixing the links

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
     console.log(currentUrl);
 
 
-    if (currentUrl == "http://127.0.0.1:5500/index.html") {
+    if (currentUrl == "https://love-to-code108.github.io/navbar/index.html") {
         const home = document.getElementsByClassName("whiteUrl");
         home[0].style.backgroundColor = "white";
         console.log(currentUrl);
     }
 
 
-    else if (currentUrl == "http://127.0.0.1:5500/profiles.html") {
+    else if (currentUrl == "https://love-to-code108.github.io/navbar/profiles.html") {
         const home = document.getElementsByClassName("whiteUrl");
         home[1].style.backgroundColor = "white";
         console.log(currentUrl);
